66c07d2b5f0a9490270ed110b7932a2dfa582be6
Jairo Lopes <73255336+JairoLopes@users.noreply.github.com>
2026-01-28 19:30:54 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,14 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image", "@vueuse/motion/nuxt", "@vueuse/nuxt", "vue3-carousel-nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@vueuse/motion/nuxt",
+    "@vueuse/nuxt",
+    "vue3-carousel-nuxt",
+  ],
   app: {
     head: {
       title: "Talles Furtado - Personal Trainer",
@@ -18,21 +25,40 @@ export default defineNuxtConfig({
         {
           name: "description",
           content:
-            "Transforme seu corpo com a metodologia do Personal Trainer Talles Furtado. Especialista em hipertrofia e emagrecimento.",
+          "Transforme seu corpo com a metodologia do Personal Trainer Talles Furtado. Especialista em hipertrofia e emagrecimento.",
         },
         {
           name: "keywords",
           content:
-            "personal trainer, hipertrofia, emagrecimento, musculação, treino, dieta, Talles Furtado",
+          "personal trainer, hipertrofia, emagrecimento, musculação, treino, dieta, Talles Furtado",
         },
-        { property: "og:title", content: "Talles Furtado - Personal Trainer" },
+        // Open Graph (WhatsApp, Facebook, LinkedIn)
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: "https://talles-personal.vercel.app" },
+        { property: "og:title", content: "Talles Furtado | Personal Trainer" },
         {
           property: "og:description",
           content:
-            "Transforme seu corpo com a metodologia do Personal Trainer Talles Furtado.",
+          "Transforme seu corpo com o Time TF. Treinos e dietas personalizadas.",
         },
-        { property: "og:image", content: "/img/Seo_img/t1.jpeg" },
-        { property: "og:type", content: "website" },
+        {
+          property: "og:image",
+          content: "https://talles-personal.vercel.app/img/Seo_img/t1.jpeg",
+        },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:image:height", content: "630" },
+
+        // Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "Talles Furtado | Personal Trainer" },
+        {
+          name: "twitter:description",
+          content: "Consultoria fitness e treinos personalizados.",
+        },
+        {
+          name: "twitter:image",
+          content: "https://talles-personal.vercel.app/img/Seo_img/t1.jpeg",
+        },
       ],
       link: [
         {
